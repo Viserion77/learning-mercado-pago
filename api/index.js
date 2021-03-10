b22c32af4b5f816f3c54c405fb0f426a47503637
Viserion77 <jeferson.a.oficial@gmail.com>
2021-03-10 21:40:07 +0000

    Features

---
+++ api/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.post("/process_payment", async function (req, res) {
+    console.log(`>post/process_payment/${JSON.stringify(req.body)}`);
+    
+
+var mercadopago = require('mercadopago');
+mercadopago.configurations.setAccessToken("TEST-783514449353831-030122-b66e9dae2a1bcf2c75e71cc217f34ece-244402927");
+
+var payment_data = {
+  transaction_amount: Number(req.body.transactionAmount),
+  token: req.body.token,
+  description: req.body.description,
+  installments: Number(req.body.installments),
+  payment_method_id: req.body.paymentMethodId,
+  issuer_id: req.body.issuer,
+  payer: {
+    email: req.body.email,
+    identification: {
+      type: req.body.docType,
+      number: req.body.docNumber
+    }
+  }
+};
+
+mercadopago.payment.save(payment_data)
+  .then(function(response) {
+    res.status(response.status).json({
+      status: response.body.status,
+      status_detail: response.body.status_detail,
+      id: response.body.id
+    });
+  })
+  .catch(function(error) {
+    res.status(response.status).send(error);
+  });
+
+    res.send({ mas: 'oque é isto?' });
+});
+
+app.listen(3000);
+console.log(`?listen/port/3000`);
