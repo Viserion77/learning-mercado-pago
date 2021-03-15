03b10f5df191f41d29444e1f19c482021b91c38f
Jeferson Alves <49992182+Viserion77@users.noreply.github.com>
2021-03-15 00:46:08 -0300

    make it works

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,140 +1,165 @@
 <template>
-  <v-container
-    >
+  <v-container>
     <v-sheet>
-    <form action="http://localhost:3000/process_payment" method="post" id="paymentForm">
-      <h3>Detalhe do comprador</h3>
-      <div>
-        <div>
-          <v-text-field label="E-mail" id="email" name="email" type="text" value="jeferson.a.oficial@gmail.com" />
-        </div>
-        <div>
-          <select
-            label="Tipo de documento"
-            id="docType"
-            name="docType"
-            data-checkout="docType"
-            type="text"
-          ></select>
-        </div>
-        <div>
-          <v-text-field
-            label="Número do documento"
-            value="10094624909"
-            id="docNumber"
-            name="docNumber"
-            data-checkout="docNumber"
-            type="text"
-          />
-        </div>
-      </div>
-      <h3>Detalhes do cartão</h3>
-      <div>
-        <div>
-          <v-text-field
-            label="Titular do cartão"
-            id="cardholderName"
-            data-checkout="cardholderName"
-            type="text"
-            value="Jeferson Alves"
-          />
-        </div>
+      <form
+        action="http://localhost:3000/process_payment"
+        method="post"
+        id="paymentForm"
+      >
+        <h3>Detalhe do comprador</h3>
         <div>
           <div>
-            <v-row><v-col>
             <v-text-field
-              label="Mês"
+              label="E-mail"
+              id="email"
+              name="email"
               type="text"
-              placeholder="MM"
-              id="cardExpirationMonth"
-              data-checkout="cardExpirationMonth"
-              onselectstart="return false"
-              onpaste="return false"
-              oncopy="return false"
-              oncut="return false"
-              ondrag="return false"
-              ondrop="return false"
-              value="11"
-              autocomplete="off"
-            /></v-col><v-col>
-            <span class="date-separator">/</span></v-col>
-            <v-col><v-text-field
-              label="Ano"
-              type="text"
-              placeholder="YY"
-              id="cardExpirationYear"
-              data-checkout="cardExpirationYear"
-              onselectstart="return false"
-              onpaste="return false"
-              oncopy="return false"
-              oncut="return false"
-              ondrag="return false"
-              ondrop="return false"
-              autocomplete="off"
-              value="25"
+              value="jeferson.a.oficial@gmail.com"
             />
-            </v-col>
-            </v-row>
+          </div>
+          <div>
+            <select
+              label="Tipo de documento"
+              id="docType"
+              name="docType"
+              data-checkout="docType"
+              type="text"
+            ></select>
+          </div>
+          <div>
+            <v-text-field
+              label="Número do documento"
+              value="10094624909"
+              id="docNumber"
+              name="docNumber"
+              data-checkout="docNumber"
+              type="text"
+            />
           </div>
         </div>
-            <v-row><v-col>
+        <h3>Detalhes do cartão</h3>
         <div>
-          <v-text-field
-            label="Número do cartão"
-            type="text"
-            id="cardNumber"
-            v-model="cardNumber"
-            @change="guessPaymentMethod()"
-            data-checkout="cardNumber"
-            onselectstart="return false"
-            onpaste="return false"
-            oncopy="return false"
-            oncut="return false"
-            ondrag="return false"
-            ondrop="return false"
-            autocomplete="off"
-          />
-        </div>
-        </v-col>
-        <v-col>
-        <div>
-          <v-text-field
-            label="Código de segurança"
-            id="securityCode"
-            data-checkout="securityCode"
-            type="text"
-            onselectstart="return false"
-            value="123"
-            onpaste="return false"
-            oncopy="return false"
-            oncut="return false"
-            ondrag="return false"
-            ondrop="return false"
-            autocomplete="off"
-          />
-        </div>
-            </v-col></v-row>
-        <div id="issuerInput">
-          <select label="Banco emissor" id="issuer" name="issuer" data-checkout="issuer"></select>
-        </div>
-        <div>
-          <select label="Parcelas" type="text" id="installments" name="installments"></select>
-        </div>
-        <div>
-          <v-text-field
-            type="hidden"
-            name="transactionAmount"
-            id="transactionAmount"
-            value="77.77"
-          />
-          <v-text-field type="hidden" name="paymentMethodId" id="paymentMethodId" />
-          <v-text-field type="hidden" name="description" id="description" />
-          <br />
-          <v-btn type="submit">Pagar</v-btn>
-          <br />
-        </div>
-      </div></form
-  ></v-sheet></v-container>
+          <div>
+            <v-text-field
+              label="Titular do cartão"
+              id="cardholderName"
+              data-checkout="cardholderName"
+              type="text"
+              value="Jeferson Alves"
+            />
+          </div>
+          <div>
+            <div>
+              <v-row
+                ><v-col>
+                  <v-text-field
+                    label="Mês"
+                    type="text"
+                    placeholder="MM"
+                    id="cardExpirationMonth"
+                    data-checkout="cardExpirationMonth"
+                    onselectstart="return false"
+                    onpaste="return false"
+                    oncopy="return false"
+                    oncut="return false"
+                    ondrag="return false"
+                    ondrop="return false"
+                    value="11"
+                    autocomplete="off" /></v-col
+                ><v-col> <span class="date-separator">/</span></v-col>
+                <v-col
+                  ><v-text-field
+                    label="Ano"
+                    type="text"
+                    placeholder="YY"
+                    id="cardExpirationYear"
+                    data-checkout="cardExpirationYear"
+                    onselectstart="return false"
+                    onpaste="return false"
+                    oncopy="return false"
+                    oncut="return false"
+                    ondrag="return false"
+                    ondrop="return false"
+                    autocomplete="off"
+                    value="25"
+                  />
+                </v-col>
+              </v-row>
+            </div>
+          </div>
+          <v-row
+            ><v-col>
+              <div>
+                <v-text-field
+                  label="Número do cartão"
+                  type="text"
+                  id="cardNumber"
+                  v-model="cardNumber"
+                  @change="guessPaymentMethod()"
+                  data-checkout="cardNumber"
+                  onselectstart="return false"
+                  onpaste="return false"
+                  oncopy="return false"
+                  oncut="return false"
+                  ondrag="return false"
+                  ondrop="return false"
+                  autocomplete="off"
+                />
+              </div>
+            </v-col>
+            <v-col>
+              <div>
+                <v-text-field
+                  label="Código de segurança"
+                  id="securityCode"
+                  data-checkout="securityCode"
+                  type="text"
+                  onselectstart="return false"
+                  value="123"
+                  onpaste="return false"
+                  oncopy="return false"
+                  oncut="return false"
+                  ondrag="return false"
+                  ondrop="return false"
+                  autocomplete="off"
+                />
+              </div> </v-col
+          ></v-row>
+          <div id="issuerInput">
+            <select
+              label="Banco emissor"
+              id="issuer"
+              name="issuer"
+              data-checkout="issuer"
+            ></select>
+          </div>
+          <div>
+            <select
+              label="Parcelas"
+              type="text"
+              id="installments"
+              name="installments"
+            ></select>
+          </div>
+          <div>
+            <v-text-field
+              type="hidden"
+              name="transactionAmount"
+              id="transactionAmount"
+              value="77.77"
+            />
+            <v-text-field
+              type="hidden"
+              name="paymentMethodId"
+              id="paymentMethodId"
+            />
+            <v-text-field type="hidden" name="description" id="description" />
+            <br />
+            <v-btn type="submit">Pagar</v-btn>
+            <br />
+          </div>
+        </div></form></v-sheet
+  ></v-container>
 </template>
 
 <script>
@@ -146,10 +171,12 @@ export default {
     req: {},
     res: {},
     response: {},
-    cardNumber: '503143321540635',
+    cardNumber: "503143321540635",
   }),
   beforeCreate() {
-    window.Mercadopago.setPublishableKey("TEST-23be4fc1-ca50-41cf-ae4d-00dcaa2ea0ee");
+    window.Mercadopago.setPublishableKey(
+      "TEST-23be4fc1-ca50-41cf-ae4d-00dcaa2ea0ee"
+    );
     window.Mercadopago.getIdentificationTypes();
   },
   methods: {
@@ -161,7 +188,9 @@ export default {
     },
     EnviePagamentoMercadoPago() {
       var mercadopago = require("mercadopago");
-      mercadopago.configurations.setAccessToken("TEST-783514449353831-030122-b66e9dae2a1bcf2c75e71cc217f34ece-244402927");
+      mercadopago.configurations.setAccessToken(
+        "TEST-783514449353831-030122-b66e9dae2a1bcf2c75e71cc217f34ece-244402927"
+      );
 
       var payment_data = {
         transaction_amount: Number(this.req.body.transactionAmount),
@@ -201,7 +230,19 @@ export default {
       }
     },
     setCardTokenAndPay(status, response) {
-      console.log(response)
+      if (status == 200 || status == 201) {
+        let form = document.getElementById("paymentForm");
+        let card = document.createElement("input");
+        card.setAttribute("name", "token");
+        //card.setAttribute("type", "hidden");
+        card.setAttribute("value", response.id);
+        console.log(response.id);
+        form.appendChild(card);
+        this.doSubmit = true;
+        form.submit();
+      } else {
+        alert("Verify filled data!\n" + JSON.stringify(response, null, 4));
+      }
     },
 
     getIssuers(paymentMethodId) {
@@ -262,6 +303,7 @@ export default {
           this.setPaymentMethod
         );
       }
+      this.CrieTokenCartao()
     },
 
     setPaymentMethod(status, response) {
